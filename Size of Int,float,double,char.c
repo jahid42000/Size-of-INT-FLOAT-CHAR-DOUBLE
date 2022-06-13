9d49e9054b90ae8557c7e49ee39604ff94a72d5b
jahid42000 <106888651+jahid42000@users.noreply.github.com>
2022-06-13 18:09:11 +0600

    Add files via upload

---
+++ Size of Int,float,double,char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char ch;
+
+    printf("Integer size is :%d\n",sizeof(i));
+    printf("Float size is :%d\n",sizeof(f));
+    printf("double size is :%d\n",sizeof(d));
+    printf("Char size is :%d\n",sizeof(ch));
+
+}
